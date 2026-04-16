add0dc88cab24ff59eda8bc8cb81b8efcf899969
gdiniscardosofyld <goncalo.cardoso@fyld.pt>
2026-04-16 15:09:11 +0100

    adjust unit test case dependencies

--- src/sitters/sitters.controller.spec.ts
+++ src/sitters/sitters.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SittersController } from './sitters.controller';
+import { SittersService } from './sitters.service';
 
 describe('SittersController', () => {
   let controller: SittersController;
@@ -7,6 +8,17 @@ describe('SittersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SittersController],
+      providers: [
+        {
+          provide: SittersService,
+          useValue: {
+            updateSitterProfile: jest.fn(),
+            getSitterProfile: jest.fn(),
+            deleteSitterProfile: jest.fn(),
+            findAll: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<SittersController>(SittersController);
--- src/sitters/sitters.service.spec.ts
+++ src/sitters/sitters.service.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SittersService } from './sitters.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('SittersService', () => {
   let service: SittersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SittersService],
+      providers: [
+        SittersService,
+        {
+          provide: PrismaService,
+          useValue: {
+            sitterProfile: {
+              upsert: jest.fn(),
+              findUnique: jest.fn(),
+              delete: jest.fn(),
+              findMany: jest.fn(),
+            },
+            user: {
+              findUnique: jest.fn(),
+              update: jest.fn(),
+            },
+            $transaction: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<SittersService>(SittersService);
